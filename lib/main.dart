f83896417928e2affd2c8ecf2ace9c2411023654
Joseca <joseca20025@gmail.com>
2023-09-26 13:10:00 -0400

    Adicion de fuentes personalizadas e implementado en el main

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(useMaterial3: true, fontFamily: 'Open Sans'),
       title: 'Cotizador',
       home: const HomePage(),
     );
