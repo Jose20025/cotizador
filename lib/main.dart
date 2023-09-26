6e898fe35d79e590f04d18b3fa25957ff7bdcf8f
Joseca <joseca20025@gmail.com>
2023-09-26 12:01:48 -0400

    Creacion de la homepage con appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
+import 'package:cotizador/views/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      title: 'Cotizador',
+      home: const HomePage(),
     );
   }
 }
