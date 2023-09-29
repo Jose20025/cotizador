ae9f0af8e16fbfde4f11892b8a83d394fc7aeb33
Joseca <joseca20025@gmail.com>
2023-09-29 10:30:40 -0400

    Implementacion de validacion y cambios en diseño de Form

--- lib/custom/input_form.dart
+++ lib/custom/input_form.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 class InputForm extends StatelessWidget {
   final double width;
   final String label;
+  final int? initialValue;
+  // final String? Function(String?)? validator;
   final int? maxLength;
   final TextInputType? type;
+  final void Function(String?)? onSave;
 
   const InputForm({
     super.key,
     this.type,
     this.maxLength,
+    this.initialValue,
+    this.onSave,
     required this.width,
     required this.label,
   });
@@ -19,8 +24,16 @@ class InputForm extends StatelessWidget {
     return SizedBox(
       width: width,
       child: TextFormField(
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Obligatorio';
+          }
+
+          return null;
+        },
         maxLength: maxLength ?? 10,
         keyboardType: type ?? type,
+        initialValue: initialValue != null ? initialValue.toString() : '',
         textAlign: TextAlign.center,
         style: const TextStyle(fontSize: 15),
         decoration: InputDecoration(
@@ -32,6 +45,7 @@ class InputForm extends StatelessWidget {
           border: const OutlineInputBorder(
               borderRadius: BorderRadius.all(Radius.circular(25))),
         ),
+        onSaved: onSave,
       ),
     );
   }
