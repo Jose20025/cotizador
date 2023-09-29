ccef4766a49115c3aae4b7eed3eb997038410874
Joseca <joseca20025@gmail.com>
2023-09-29 17:35:56 -0400

    Cambios en la CotizacionTile

--- lib/custom/cotizacion_tile.dart
+++ lib/custom/cotizacion_tile.dart
@@ -1,5 +1,8 @@
 import 'package:cotizador/models/cotizacion.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+NumberFormat numberFormat = NumberFormat.decimalPattern('es_ES');
 
 class CotizacionCard extends StatelessWidget {
   final Cotizacion cotizacion;
@@ -11,9 +14,22 @@ class CotizacionCard extends StatelessWidget {
       children: [
         Card(
           child: ListTile(
-            title: Text(cotizacion.montoTotal.toString()),
-            subtitle: Text('Cuotas: \$${cotizacion.cuotas}/año'),
-          ),
+              title: Center(
+                  child: Text('Monto Total: ${cotizacion.montoTotal}\$')),
+              subtitle: Column(
+                children: [
+                  Text(
+                      'Cuota: ${numberFormat.format(cotizacion.montoCuotas)}\$/mes'),
+                  Text(
+                      'Interés: ${numberFormat.format(cotizacion.interes * 100)}%'),
+                  cotizacion.cuotaInicial == null
+                      ? const SizedBox()
+                      : Text(
+                          'Cuota Inicial: ${numberFormat.format(cotizacion.cuotaInicial)}\$'),
+                  Text(
+                      'Tiempo: ${numberFormat.format(cotizacion.tiempo)} ${cotizacion.tiempo > 1 ? "años" : "año"} | ${numberFormat.format(cotizacion.tiempo * 12)} ${(cotizacion.tiempo * 24) > 1 ? "meses" : "mes"}'),
+                ],
+              )),
         ),
         const Divider()
       ],
