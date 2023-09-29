4cb0606408547a2d6f6fe8e3730a63f45a98ff4b
Joseca <joseca20025@gmail.com>
2023-09-29 17:16:26 -0400

    Cambios en la clase Cotizacion

--- lib/models/cotizacion.dart
+++ lib/models/cotizacion.dart
@@ -3,12 +3,29 @@ class Cotizacion {
   double? cuotaInicial;
   double montoTotal;
   double tiempo;
-  double cuotas;
+  double montoCuotas;
 
   Cotizacion(
       {required this.interes,
       this.cuotaInicial,
       required this.montoTotal,
-      required this.cuotas,
+      required this.montoCuotas,
       required this.tiempo});
+
+  factory Cotizacion.fromJson(Map<String, dynamic> json) {
+    return Cotizacion(
+        interes: json['interes'],
+        montoTotal: json['montoTotal'],
+        montoCuotas: json['montoCuotas'],
+        tiempo: json['tiempo'],
+        cuotaInicial: json['cuotaInicial']);
+  }
+
+  Map<String, dynamic> toJson() => {
+        'montoTotal': montoTotal,
+        'interes': interes,
+        'cuotaInicial': cuotaInicial,
+        'tiempo': tiempo,
+        'montoCuotas': montoCuotas,
+      };
 }
