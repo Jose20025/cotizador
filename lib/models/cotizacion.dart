a66b709de5f0e773fcaedd7422d3edf1756ba44e
Joseca <joseca20025@gmail.com>
2023-09-29 13:36:44 -0400

    Cambios en la clase Cotizacion

--- lib/models/cotizacion.dart
+++ lib/models/cotizacion.dart
@@ -1,16 +1,14 @@
 class Cotizacion {
   double interes;
-  double cuotaInicial;
+  double? cuotaInicial;
   double montoTotal;
   double tiempo;
   double cuotas;
-  int cantidadCuotas;
 
   Cotizacion(
       {required this.interes,
-      required this.cuotaInicial,
+      this.cuotaInicial,
       required this.montoTotal,
       required this.cuotas,
-      required this.cantidadCuotas,
       required this.tiempo});
 }
