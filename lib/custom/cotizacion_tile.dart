4929ec01a1a9df8369931426c9b34d8047093e95
Joseca <joseca20025@gmail.com>
2023-09-30 13:23:52 -0400

    CotizacionTile adaptada

--- lib/custom/cotizacion_tile.dart
+++ lib/custom/cotizacion_tile.dart
@@ -8,30 +8,37 @@ class CotizacionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Card(
-          child: ListTile(
-              title: Center(
-                  child: Text(
-                      'Monto Total: ${NumberFormat.currency().format(cotizacion.montoTotal)}')),
-              subtitle: Column(
-                children: [
-                  Text(
-                      'Importe de Cuotas: ${NumberFormat.currency().format(cotizacion.importeCuotas)}/mes'),
-                  Text(
-                      'Precio por m²: ${NumberFormat.currency().format(cotizacion.precioMetroCuadrado)}'),
-                  cotizacion.cuotaInicial == null
-                      ? const SizedBox()
-                      : Text(
-                          'Cuota Inicial: ${NumberFormat.currency().format(cotizacion.cuotaInicial)}'),
-                  Text(
-                      'Tiempo: ${NumberFormat.decimalPatternDigits().format(cotizacion.tiempo)} ${cotizacion.tiempo > 1 ? "meses" : "mes"} | ${NumberFormat.decimalPatternDigits().format(cotizacion.tiempo / 12)} ${(cotizacion.tiempo / 12) > 1 ? "años" : "año"}'),
-                ],
-              )),
-        ),
-        const Divider()
-      ],
+    return Container(
+      margin: const EdgeInsets.all(5.0),
+      child: Column(
+        children: [
+          Card(
+            child: ListTile(
+                title: Center(
+                    child: Text(
+                        'Import de Cuotas: ${NumberFormat.currency().format(cotizacion.importeCuotas)}/mes')),
+                subtitle: Column(
+                  children: [
+                    Text(
+                        'Monto a Pagar: ${NumberFormat.currency().format(cotizacion.montoPagar)}'),
+                    Text(
+                        'Superficie del Lote: ${NumberFormat.decimalPatternDigits().format(cotizacion.superficie)} m²'),
+                    Text(
+                        'Mantenimiento: ${NumberFormat.currency().format(cotizacion.mantenimiento)}'),
+                    Text(
+                        'Precio por m²: ${NumberFormat.currency().format(cotizacion.precioMetroCuadrado)}'),
+                    cotizacion.cuotaInicial == null
+                        ? const SizedBox()
+                        : Text(
+                            'Cuota Inicial: ${NumberFormat.currency().format(cotizacion.cuotaInicial)}'),
+                    Text(
+                        'Tiempo: ${NumberFormat.decimalPatternDigits().format(cotizacion.tiempo)} ${cotizacion.tiempo > 1 ? "meses" : "mes"} | ${NumberFormat.decimalPatternDigits().format(cotizacion.tiempo / 12)} ${(cotizacion.tiempo / 12) > 1 ? "años" : "año"}'),
+                  ],
+                )),
+          ),
+          const Divider()
+        ],
+      ),
     );
   }
 }
