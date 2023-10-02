10a47064c49b05519138c36d9a0df0a39969a323
Joseca <joseca20025@gmail.com>
2023-10-02 10:43:18 -0400

    Creacion de la CotizacionClientePage

--- lib/views/cotizacion_cliente_page.dart
+++ lib/views/cotizacion_cliente_page.dart
@@ -3,10 +3,10 @@ import '../models/cotizacion.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class CotizacionPage extends StatelessWidget {
+class CotizacionClientePage extends StatelessWidget {
   final Cotizacion cotizacion;
 
-  const CotizacionPage({super.key, required this.cotizacion});
+  const CotizacionClientePage({super.key, required this.cotizacion});
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +21,12 @@ class CotizacionPage extends StatelessWidget {
           child: Column(
             children: [
               CustomCard(
+                icon: Icons.monetization_on_outlined,
+                title: 'Importe de Cuota',
+                subtitle:
+                    NumberFormat.currency().format(cotizacion.importeCuotas),
+              ),
+              CustomCard(
                 title: 'Superficie del Lote',
                 subtitle:
                     '${NumberFormat.decimalPatternDigits().format(cotizacion.superficie)} m²',
@@ -34,7 +40,7 @@ class CotizacionPage extends StatelessWidget {
               ),
               CustomCard(
                 title: 'Monto Total',
-                subtitle: NumberFormat.currency().format(cotizacion.montoTotal),
+                subtitle: NumberFormat.currency().format(cotizacion.montoPagar),
                 icon: Icons.monetization_on_outlined,
               ),
               cotizacion.cuotaInicial != null
@@ -45,27 +51,10 @@ class CotizacionPage extends StatelessWidget {
                       icon: Icons.monetization_on_outlined)
                   : const SizedBox(),
               CustomCard(
-                icon: Icons.monetization_on_outlined,
-                title: 'Monto a Pagar',
-                subtitle: NumberFormat.currency().format(cotizacion.montoPagar),
-              ),
-              CustomCard(
                 icon: Icons.access_alarm_outlined,
                 title: 'Tiempo',
                 subtitle:
                     '${NumberFormat.decimalPatternDigits().format(cotizacion.tiempo / 12)} ${cotizacion.tiempo > 1 ? "años" : "año"} | ${NumberFormat.decimalPatternDigits().format(cotizacion.tiempo)} ${(cotizacion.tiempo * 24) > 1 ? "meses" : "mes"}',
-              ),
-              CustomCard(
-                title: 'Mantenimiento',
-                subtitle:
-                    NumberFormat.currency().format(cotizacion.mantenimiento),
-                icon: Icons.monetization_on_outlined,
-              ),
-              CustomCard(
-                icon: Icons.monetization_on_outlined,
-                title: 'Importe de Cuota',
-                subtitle:
-                    NumberFormat.currency().format(cotizacion.importeCuotas),
               ),
             ],
           ),
