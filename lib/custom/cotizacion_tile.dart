40c4f0712967adb71ee80082e701adffc99b0a6b
Joseca <joseca20025@gmail.com>
2023-10-06 00:00:00 -0400

    PDF desde historial

--- lib/custom/cotizacion_tile.dart
+++ lib/custom/cotizacion_tile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:open_file/open_file.dart';
 
 import '../models/cotizacion.dart';
+import '../utils/pdf_generator.dart';
 import 'mini_card.dart';
 
 class CotizacionCard extends StatelessWidget {
@@ -12,72 +14,81 @@ class CotizacionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.symmetric(horizontal: 10),
-        child: Card(
-          elevation: 4,
-          child: ListTile(
-            title: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Center(
-                            child: Text(
-                          DateFormat('dd/MMM/yyyy').format(cotizacion.fecha!),
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        )),
-                      ),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Card(
+        elevation: 4,
+        child: ListTile(
+          title: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Center(
+                          child: Text(
+                        DateFormat('dd/MMM/yyyy').format(cotizacion.fecha!),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      )),
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 6,
-                ),
-                Center(
-                  child: Text(
-                    'Cotización para:\t\t\t\t\t\t ${cotizacion.referencia}',
-                    style: const TextStyle(
-                        fontSize: 17, fontWeight: FontWeight.bold),
                   ),
-                )
-              ],
-            ),
-            subtitle: Column(
-              children: [
-                const Divider(),
-                MiniCard(
-                    title: 'Monto a Pagar',
-                    subtitle:
-                        NumberFormat.currency().format(cotizacion.montoPagar)),
-                MiniCard(
-                    title: 'Mantenimiento',
-                    subtitle: NumberFormat.currency()
-                        .format(cotizacion.mantenimiento)),
-                MiniCard(
-                  title: 'Tiempo',
-                  subtitle:
-                      '${NumberFormat.decimalPatternDigits().format(cotizacion.tiempo / 12)} ${(cotizacion.tiempo / 12) == 1 ? "año" : "años"} | ${NumberFormat.decimalPatternDigits().format(cotizacion.tiempo)} ${(cotizacion.tiempo) == 1 ? "mes" : "meses"}',
+                ],
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Center(
+                child: Text(
+                  'Cotización para:\t\t\t\t\t\t ${cotizacion.referencia}',
+                  style: const TextStyle(
+                      fontSize: 17, fontWeight: FontWeight.bold),
                 ),
-                MiniCard(
-                  title: 'Superficie del Lote',
-                  subtitle:
-                      NumberFormat.currency().format(cotizacion.superficie),
-                ),
-                MiniCard(
-                  title: 'Precio del m²',
-                  subtitle: NumberFormat.currency()
-                      .format(cotizacion.precioMetroCuadrado),
-                ),
-                MiniCard(
-                    title: 'Importe de Cuotas',
-                    subtitle: NumberFormat.currency()
-                        .format(cotizacion.importeCuotas)),
-              ],
-            ),
+              )
+            ],
           ),
-        ));
+          subtitle: Column(
+            children: [
+              const Divider(),
+              MiniCard(
+                title: 'Monto Total',
+                subtitle: NumberFormat.currency().format(cotizacion.montoPagar),
+              ),
+              MiniCard(
+                title: 'Tiempo',
+                subtitle:
+                    '${NumberFormat.decimalPatternDigits().format(cotizacion.tiempo / 12)} ${(cotizacion.tiempo / 12) == 1 ? "año" : "años"} | ${NumberFormat.decimalPatternDigits().format(cotizacion.tiempo)} ${(cotizacion.tiempo) == 1 ? "mes" : "meses"}',
+              ),
+              MiniCard(
+                title: 'Superficie del Lote',
+                subtitle: NumberFormat.currency().format(cotizacion.superficie),
+              ),
+              MiniCard(
+                title: 'Precio del m²',
+                subtitle: NumberFormat.currency()
+                    .format(cotizacion.precioMetroCuadrado),
+              ),
+              MiniCard(
+                title: 'Importe de Cuotas',
+                subtitle:
+                    NumberFormat.currency().format(cotizacion.importeCuotas),
+              ),
+              const Divider(),
+              ElevatedButton(
+                  style: const ButtonStyle(
+                      surfaceTintColor:
+                          MaterialStatePropertyAll(Colors.transparent)),
+                  onPressed: () async {
+                    final archivo =
+                        await PDFGenerator.generatePDF(cotizacion: cotizacion);
+
+                    await OpenFile.open(archivo.path);
+                  },
+                  child: const Text('Exportar a PDF'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
