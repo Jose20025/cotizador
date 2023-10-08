56362666dfbf260ca39bea5b61ef1eb0b2e78c16
Joseca <joseca20025@gmail.com>
2023-10-08 12:07:51 -0400

    Tildes en los proyectos

--- lib/utils/proyectos.dart
+++ lib/utils/proyectos.dart
@@ -1,10 +1,10 @@
 const Map<int, String> proyectos = {
   0: 'Cartagena 1',
   1: 'Cartagena 2',
-  2: 'Mana 1',
-  3: 'Mana 2',
-  4: 'Mana 3',
-  5: 'Mana 4',
-  6: 'Mana 5',
-  7: 'Mana 6',
+  2: 'Maná 1',
+  3: 'Maná 2',
+  4: 'Maná 3',
+  5: 'Maná 4',
+  6: 'Maná 5',
+  7: 'Maná 6',
 };
