c2cf22e1d15055b3f1ee9de395ec2afdbb0b1e08
Jose Carlos <joseca20025@gmail.com>
2023-10-10 15:56:08 -0400

    Creacion de los tipos de errores

--- lib/utils/cotizaciones.dart
+++ lib/utils/cotizaciones.dart
@@ -1,9 +1,29 @@
 import '../models/cotizacion.dart';
 import 'proyectos.dart';
 
+class EqualError implements Exception {
+  String message;
+  EqualError(this.message);
+
+  @override
+  String toString() => message;
+}
+
+class HigherFirstPaymentError implements Exception {
+  String message;
+  HigherFirstPaymentError(this.message);
+
+  @override
+  String toString() => message;
+}
+
 Cotizacion crearCotizacion(double precioMetroCuadrado, double superficie,
     double cuotaInicial, int tiempo, String referencia, int numeroProyecto) {
   double montoTotal = superficie * precioMetroCuadrado;
+
+  if (cuotaInicial == montoTotal) {
+    throw EqualError('La cuota inicial es igual al monto total');
+  }
 
   montoTotal -= cuotaInicial;
 
@@ -12,7 +32,8 @@ Cotizacion crearCotizacion(double precioMetroCuadrado, double superficie,
   final montoPagar = (montoTotal + mantenimiento);
 
   if (cuotaInicial > montoPagar) {
-    throw Exception('La cuota inicial no debe ser mayor al monto del lote');
+    throw HigherFirstPaymentError(
+        'La cuota inicial no debe ser mayor al monto del lote');
   }
 
   final importeCuotas = montoPagar / tiempo;
